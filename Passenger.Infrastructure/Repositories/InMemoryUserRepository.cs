1cdcfc58ef8a5f7341cc07229061e68dfd8c2107
Mieszko Cichon <svider.mc@gmail.com>
2017-06-16 15:24:07 +0200

    Node object rebuild.

--- Passenger.Infrastructure/Repositories/InMemoryUserRepository.cs
+++ Passenger.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Passenger.Core.Domain;
 using Passenger.Core.Repositories;
